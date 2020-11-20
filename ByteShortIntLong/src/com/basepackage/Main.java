1f35254bfbf0076a833e4a99fd4159949774b9db
Arturs Svikis <arturs.svikis@gmail.com>
2020-11-20 21:00:42 +0100

    learning java

---
+++ ByteShortIntLong/src/com/basepackage/Main.java
@@ -0,0 +1,18 @@
+package com.basepackage;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted MAX Value = " + (myMaxIntValue + 1)); // Overflow
+        System.out.println("Busted MIN Value = " + (myMinIntValue - 1)); // Underflow
+
+        int myMaxIntTest = 2_147_483_647;
+    }
+}
