e758b2386d124485d1dc4aed2adc662dfdc1359f
Arturs Svikis <arturs.svikis@gmail.com>
2020-11-21 12:51:24 +0100

    float and double

---
+++ FloatAndDouble/src/com/basepackage/Main.java
@@ -0,0 +1,22 @@
+package com.basepackage;
+
+public class Main {
+
+    public static void main(String[] args) {
+	
+        
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Minimum Float Value = " + myMinFloatValue);
+        System.out.println("Maximum Float Value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Minimum Double Value = " + myMinDoubleValue);
+        System.out.println("Maximum Double Value = " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5.25f;
+        double myDoubleValue = 5.25d;
+    }
+}
