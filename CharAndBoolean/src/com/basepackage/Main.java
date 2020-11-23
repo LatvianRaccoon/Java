26aadbd3ac71ec6677f767bc59f53f067f3ead2f
Arturs Svikis <arturs.svikis@gmail.com>
2020-11-23 18:57:54 +0100

    char and booleans

---
+++ CharAndBoolean/src/com/basepackage/Main.java
@@ -0,0 +1,20 @@
+package com.basepackage;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'A';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        boolean isCustomerOverTwentyOne = true;
+
+    }
+}
