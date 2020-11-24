84f7ff0fa29feb5011c98c1897f4564acbd3bb98
Arturs Svikis <arturs.svikis@gmail.com>
2020-11-24 19:10:48 +0100

    logical and operator

--- IfThen/src/com/basepackage/Main.java
+++ IfThen/src/com/basepackage/Main.java
@@ -9,5 +9,15 @@ public class Main {
             System.out.println("It is not an Alien!");
             System.out.println("And I am scared.");
         }
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than second and less than 100");
+        }
     }
 }
