a67d21596664157ed913623d41b1900b0be99d0d
Arturs Svikis <arturs.svikis@gmail.com>
2020-11-24 19:00:05 +0100

    if statement

---
+++ IfThen/src/com/basepackage/Main.java
@@ -0,0 +1,13 @@
+package com.basepackage;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an Alien!");
+            System.out.println("And I am scared.");
+        }
+    }
+}
