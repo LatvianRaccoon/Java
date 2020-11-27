46918e54c6c886df49878c6fc730d481145cdc9a
Arturs Svikis <arturs.svikis@gmail.com>
2020-11-27 17:55:12 +0100

    statements

---
+++ Statements/src/com/basepackage/Main.java
@@ -0,0 +1,21 @@
+package com.basepackage;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myVariable = 50;
+
+        myVariable++;
+        myVariable--;
+
+        System.out.println("This is test!");
+
+        System.out.println("This is" +
+                " another" +
+                " still more.");
+
+        int anotherVariable = 50;myVariable--;System.out.println("This is another one");
+        // not good practice!!!
+    }
+}
