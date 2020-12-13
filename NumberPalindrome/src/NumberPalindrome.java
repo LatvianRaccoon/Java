5b8032a3ad71231185f06acfb7eac6701ca1f947
Arturs Svikis <arturs.svikis@gmail.com>
2020-12-13 18:19:17 +0100

    Palindrome number challenge

---
+++ NumberPalindrome/src/NumberPalindrome.java
@@ -0,0 +1,21 @@
+public class NumberPalindrome {
+    public static boolean isPalindrome(int number) {
+        int copyNumber = 0;
+        int currentDigit = 0;
+        int reversedNumber = 0;
+        copyNumber = number;
+
+        do {
+            currentDigit = copyNumber % 10;
+            reversedNumber = (reversedNumber * 10) + currentDigit;
+            copyNumber /= 10;
+        } while (copyNumber != 0);
+            if (number == reversedNumber) {
+                return true;
+            } else {
+                return false;
+            }
+
+
+    }
+}
