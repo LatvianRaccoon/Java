01de68ec3abf460cc64fbee27eece425bc565a36
Arturs Svikis <arturs.svikis@gmail.com>
2020-12-13 17:51:25 +0100

    Sum of digits challenge

---
+++ SumDigitsChallenge/src/com/basepackage/Main.java
@@ -0,0 +1,28 @@
+package com.basepackage;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("The sum of the digits of number = " + sumDigits(123)  );
+
+    }
+
+    public static int sumDigits(int number) {
+        if (number < 10) {
+            return -1;
+        }
+
+        int sum = 0;
+
+        while(number > 0) {
+
+            int digit = number % 10;
+            sum += digit;
+
+            number /= 10;
+        }
+
+        return sum;
+    }
+}
