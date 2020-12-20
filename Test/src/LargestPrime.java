f7725861fe49e78d51e8223ef8c563af12be723b
Arturs Svikis <arturs.svikis@gmail.com>
2020-12-20 15:52:42 +0100

    Largest prime factor

---
+++ Test/src/LargestPrime.java
@@ -0,0 +1,18 @@
+public class LargestPrime {
+    public static int getLargestPrime(int number) {
+        if (number <= 1) {
+            return -1;
+        } else {
+            int div = 2;
+            while (div < number) {
+                if (number % div != 0) {
+                    div++;
+                } else {
+                    number = number / div;
+                    div = 2;
+                }
+            }
+            return number;
+        }
+    }
+}
